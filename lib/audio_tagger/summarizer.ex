08bbfe8b15de7a34d46a29e8c61d5a1858bf5a38
Noah Settersten <noah@headway.io>
2024-01-08 14:50:40 -0600

    chore: Add stub for `Summarizer` module

---
+++ lib/audio_tagger/summarizer.ex
@@ -0,0 +1,4 @@
+defmodule AudioTagger.Summarizer do
+  # TODO: Summarize transcribed text.
+  # For medical text, this model may prove useful: https://huggingface.co/Falconsai/medical_summarization
+end
